88cb3bbce5228ad4e40f9063544c07949f3908df
GuilleLink <32148569+GuilleLink@users.noreply.github.com>
2018-04-11 00:56:16 -0600

    Cambios al programa, ya solo un error de cámara
    Cambios, ya funciona gran parte del programa, simplemente problema con la cámara

---
+++ Assets/Scripts/BirdScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour {
+
+    float jumpForce = 200f;
+    float forwardSpeed = 2f;
+    Rigidbody2D rb;
+    public GameObject cam;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Start");
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = Vector2.zero;
+            rb.AddForce(Vector2.up*jumpForce);
+        }
+        rb.transform.Translate(new Vector3(1, 0, 0) * forwardSpeed * Time.deltaTime);
+        cam.transform.Translate(new Vector3(1, 0, 0) * forwardSpeed * Time.deltaTime);
+    }
+}
