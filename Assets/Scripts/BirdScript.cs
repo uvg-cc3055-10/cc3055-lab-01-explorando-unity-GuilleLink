c6d9731f78ec66f269029d3d99ae4ad14561333f
GuilleLink <32148569+GuilleLink@users.noreply.github.com>
2018-04-11 08:19:19 -0600

    Proyecto FLappy Bird terminado
    El juego de Flappy bird esta terminado, ya funciona la camara y la muerte

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -6,6 +6,7 @@ public class BirdScript : MonoBehaviour {
 
     float jumpForce = 200f;
     float forwardSpeed = 2f;
+    bool dead = false;
     Rigidbody2D rb;
     public GameObject cam;
 
@@ -14,15 +15,27 @@ public class BirdScript : MonoBehaviour {
         Debug.Log("Start");
         rb = GetComponent<Rigidbody2D>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetButtonDown("Jump"))
-        {
-            rb.velocity = Vector2.zero;
-            rb.AddForce(Vector2.up*jumpForce);
+
+    // Update is called once per frame
+    void Update() {
+        if (dead == false) { 
+            if (Input.GetButtonDown("Jump"))
+            {
+                rb.velocity = Vector2.zero;
+                rb.AddForce(Vector2.up * jumpForce);
+            }
+            rb.transform.Translate(new Vector3(1, 0, 0) * forwardSpeed * Time.deltaTime);
+            cam.transform.Translate(new Vector3(1, 0, 0) * forwardSpeed * Time.deltaTime);
         }
-        rb.transform.Translate(new Vector3(1, 0, 0) * forwardSpeed * Time.deltaTime);
-        cam.transform.Translate(new Vector3(1, 0, 0) * forwardSpeed * Time.deltaTime);
+        if (rb.transform.position.x >= 40)
+        {
+            dead = true;
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        dead = true;
+        cam.transform.Translate(new Vector3(0, 0, 0) * forwardSpeed * Time.deltaTime);
     }
 }
